6629d7742752ddfed6554851b87a28f9bd6a3626
Andre Vitor Rampanelli <andrerampanelli1@gmail.com>
2021-07-12 21:33:31 -0300

    sync wait return

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
+
 	producer := NewKafkaProducer()
 
-	Publish("Manda ai", "gopics", producer, nil)
+	Publish("Manda ai", "gopics", producer, nil, deliveryChan)
+
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	} else {
+		fmt.Println("Mensagem enviada: ", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -26,14 +38,14 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
